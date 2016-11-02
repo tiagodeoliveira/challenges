2408ef50f2e1e1a8d5ee7faaf5241c90a91ccae2
Tiago Oliveira <tiago.oliveira@gmx.us>
2016-11-02 17:59:23 -0200

    Adding an edge case example.

--- circular_array_rotation.erl
+++ circular_array_rotation.erl
@@ -4,21 +4,18 @@
 main() ->
     { ok, [N, K, Q] } = io:fread("", "~d~d~d"),
     A = read_array(N, "~d"),
-    Q_list = lists:seq(1, Q),
-    lists:foreach(
-      fun(Q0) ->
-        { ok, [M] } = io:fread("", "~d")
-      end,
-      Q_list),
-      case N rem K ->
-        0 ->
-
+    Q_list = read_array(Q, "~d"),
+    M = N rem K,
+    print_list(Q_list, M, A),
     true.
 
-rotate(A, K) ->
+print_list([], _M, _Arr) -> true;
+print_list([H|T], M, Arr) ->
+  P = (H + M) rem length(Arr),
+  io:fwrite("~p~n", [lists:nth(P + 1, Arr)]),
+  print_list(T, M, Arr).
 
-
-read_array(0,D) -> [];
+read_array(0,_D) -> [];
 read_array(N,D) ->
    {ok, [X]} = io:fread("", D),
    [X | read_array(N-1,D)].
