6c56636b6d61737589950b07bb71edc88a43ef61
Tiago Oliveira <tiago.oliveira@gmx.us>
2016-11-03 22:54:49 -0200

    Solving array rotation. The code can be improved.

--- circular_array_rotation.erl
+++ circular_array_rotation.erl
@@ -3,19 +3,26 @@
 
 main() ->
     { ok, [N, K, Q] } = io:fread("", "~d~d~d"),
-    A = read_array(N, "~d"),
-    Q_list = read_array(Q, "~d"),
-    M = N rem K,
+    { ok, A } = read_array(N, "~d"),
+    { ok, Q_list } = read_array(Q, "~d"),
+    M = K rem N, % get the rotation module/factor
     print_list(Q_list, M, A),
     true.
 
 print_list([], _M, _Arr) -> true;
 print_list([H|T], M, Arr) ->
-  P = (H + M) rem length(Arr),
-  io:fwrite("~p~n", [lists:nth(P + 1, Arr)]),
+  Arr_size = length(Arr),
+  Pos = if
+    M =:= 0 -> H + 1;
+    true -> (H + 1 + (Arr_size - M))
+  end,
+  P = if
+    Pos > length(Arr) -> Pos rem length(Arr);
+    true -> Pos
+  end,
+  io:fwrite("Printing position ~p with module ~p with array = ~p IS: ~p~n", [H, M, length(Arr), P]),
+  io:fwrite("~p~n", [lists:nth(P, Arr)]),
   print_list(T, M, Arr).
 
-read_array(0,_D) -> [];
-read_array(N,D) ->
-   {ok, [X]} = io:fread("", D),
-   [X | read_array(N-1,D)].
+read_array(Size, Format) -> io:fread("", repeat(Format, Size)).
+repeat(Str, Times) -> lists:flatten(lists:duplicate(Times, Str)).
